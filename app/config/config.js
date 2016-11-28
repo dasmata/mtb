aef06c624ec9a9c9bdd229d020a6ef4ec9c6f6d2
Tiberiu Popovici <tiberiu.popovici@zitec.com>
2016-11-28 07:32:37 +0200

    added the env and cofig for the app

---
+++ app/config/config.js
@@ -0,0 +1,9 @@
+"use strict";
+var local = require("./config.local.js");
+
+module.exports = {
+    "development": {
+        "client_js": "http://127.0.0.1:8080/js/",
+        "database": local
+    }
+};
---
+++ app/config/config.local.js.sample.js
@@ -0,0 +1,7 @@
+"use strict";
+module.exports = {
+    "host" : "127.0.0.1",
+    "user": "mtb",
+    "password": "|oo54sn_vsl72n*5",
+    "dbname": "mtb"
+};
